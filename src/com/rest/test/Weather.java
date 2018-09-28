35f15aab126c081aa7f8716f43ad0c23249d4ed0
gaura <gaurav.cap01@gmail.com>
2018-09-27 23:31:19 -0500

    Rest Assured V1

---
+++ src/com/rest/test/Weather.java
@@ -0,0 +1,239 @@
+package com.rest.test;
+
+
+
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+
+public class Weather {
+	
+	@Test
+	public void GetWeatherDetails()
+	{
+		// Specify the base URL to the RESTful web service
+		RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+
+		// Get the RequestSpecification of the request that you want to sent
+		// to the server. The server is specified by the BaseURI that we have
+		// specified in the above step.
+		RequestSpecification httpRequest = RestAssured.given();
+
+		// Make a GET request call directly by using RequestSpecification.get() method.
+		// Make sure you specify the resource name.
+		Response response = httpRequest.get("/Hyderabad");
+
+		// Response.asString method will directly return the content of the body
+		// as String.
+		System.out.println("Response Body is =>  " + response.asString());
+	}
+	
+	@Test
+	public void GetWeatherDetailsShort()
+	{
+		RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.get("/Hyderabad");
+
+		// Get the status code from the Response. In case of 
+		// a successfull interaction with the web service, we
+		// should get a status code of 200.
+		int statusCode = response.getStatusCode();
+
+		// Assert that correct status code is returned.
+		Assert.assertEquals(statusCode /*actual value*/, 200 /*expected value*/, "Correct status code returned");
+	}
+	
+	@Test
+	public void GetWeatherDetailsInvalidCity()
+	{
+		RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.get("/78789798798");
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(statusCode /*actual value*/, 200 /*expected value*/, "Correct status code returned");
+	}
+	
+	@Test
+	public void GetWeatherStatusLine()
+	{
+		RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.get("/Hyderabad");
+		
+		// Get the status line from the Response and store it in a variable called statusLine
+		String statusLine = response.getStatusLine();
+		Assert.assertEquals(statusLine /*actual value*/, "HTTP/1.1 200 OK" /*expected value*/, "Correct status code returned");
+	}
+	
+	@Test
+	public void GetWeatherHeaders()
+	{
+		RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.get("/Hyderabad");
+
+		// Reader header of a give name. In this line we will get
+		// Header named Content-Type
+		String contentType = response.header("Content-Type");
+		System.out.println("Content-Type value: " + contentType);
+
+		// Reader header of a give name. In this line we will get
+		// Header named Server
+		String serverType =  response.header("Server");
+		System.out.println("Server value: " + serverType);
+
+		// Reader header of a give name. In this line we will get
+		// Header named Content-Encoding
+		String acceptLanguage = response.header("Content-Encoding");
+		System.out.println("Content-Encoding: " + acceptLanguage);
+	}
+	
+	@Test
+	public void IteratingOverHeaders()
+	{
+		RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.get("/Hyderabad");
+
+		// Get all the headers. Return value is of type Headers.
+		// Headers class implements Iterable interface, hence we
+		// can apply an advance for loop to go through all Headers
+		// as shown in the code below
+		Headers allHeaders = response.headers();
+
+		// Iterate over all the Headers
+		for(Header header : allHeaders)
+		{
+			System.out.println("Key: " + header.getName() + " Value: " + header.getValue());
+		}
+	}
+	
+	//@Test
+	public void GetWeatherHeadersAssert()
+	{
+		RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.get("/Hyderabad");
+
+		// Reader header of a give name. In this line we will get
+		// Header named Content-Type
+		String contentType = response.header("Content-Type");
+		Assert.assertEquals(contentType /* actual value */, "application/json" /* expected value */);
+
+		// Reader header of a give name. In this line we will get
+		// Header named Server
+		String serverType =  response.header("Server");
+		Assert.assertEquals(serverType /* actual value */, "nginx/1.12.1" /* expected value */);
+
+		// Reader header of a give name. In this line we will get
+		// Header named Content-Encoding
+		String contentEncoding = response.header("Content-Encoding");
+		Assert.assertEquals(contentEncoding /* actual value */, "gzip" /* expected value */);
+	}
+	
+	
+	@Test
+	public void WeatherMessageBody()
+	{
+		RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.get("/Hyderabad");
+
+		// Retrieve the body of the Response
+		ResponseBody body = response.getBody();
+
+		// To check for sub string presence get the Response body as a String.
+		// Do a String.contains
+		String bodyAsString = body.asString();
+
+		// convert the body into lower case and then do a comparison to ignore casing.
+		Assert.assertEquals(bodyAsString.toLowerCase().contains("hyderabad") /*Expected value*/, true /*Actual Value*/, "Response body contains Hyderabad");
+	}
+	
+	@Test
+	public void VerifyCityInJsonResponse()
+	{
+		RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.get("/Hyderabad");
+
+		// First get the JsonPath object instance from the Response interface
+		JsonPath jsonPathEvaluator = response.jsonPath();
+
+		// Then simply query the JsonPath object to get a String value of the node
+		// specified by JsonPath: City (Note: You should not put $. in the Java code)
+		String city = jsonPathEvaluator.get("City");
+
+		// Let us print the city variable to see what we got
+		System.out.println("City received from Response " + city);
+
+		// Validate the response
+		Assert.assertEquals(city, "Hyderabad", "Correct city name received in the Response");
+
+	}
+	
+	@Test
+	public void DisplayAllNodesInWeatherAPI()
+	{
+		RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.get("/Hyderabad");
+		System.out.println(response.asString());
+		// First get the JsonPath object instance from the Response interface
+		JsonPath jsonPathEvaluator = response.jsonPath();
+		
+
+		// Let us print the city variable to see what we got
+		System.out.println("City received from Response " + jsonPathEvaluator.get("City"));
+
+		// Print the temperature node
+		System.out.println("Temperature received from Response " + jsonPathEvaluator.get("Temperature"));
+
+		// Print the humidity node
+		System.out.println("Humidity received from Response " + jsonPathEvaluator.get("Humidity"));
+
+		// Print weather description
+		System.out.println("Weather description received from Response " + jsonPathEvaluator.get("Weather"));
+
+		// Print Wind Speed
+		System.out.println("City received from Response " + jsonPathEvaluator.get("WindSpeed"));
+
+		// Print Wind Direction Degree
+		System.out.println("City received from Response " + jsonPathEvaluator.get("WindDirectionDegree"));
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void RegistrationSuccessful()
+	{		
+		RestAssured.baseURI ="http://restapi.demoqa.com/customer";
+		RequestSpecification request = RestAssured.given();
+
+		JSONObject requestParams = new JSONObject();
+		requestParams.put("FirstName", "Virender"); // Cast
+		requestParams.put("LastName", "Singh");
+		requestParams.put("UserName", "sdimpleuser2dd2011");
+		requestParams.put("Password", "password1");
+
+		requestParams.put("Email",  "sample2ee26d9@gmail.com");
+		request.body(requestParams.toJSONString());
+		Response response = request.post("/register");
+
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(statusCode, "200");
+		String successCode = response.jsonPath().get("SuccessCode");
+		Assert.assertEquals( "Correct Success code was returned", successCode, "OPERATION_SUCCESS");
+	}
+	
+	
+
+}
